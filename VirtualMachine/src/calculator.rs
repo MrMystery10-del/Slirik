9c042f2b303f8f6a887bef8e8d5941a0b9a6ce9d
David-Killian <killianda36@gmail.com>
2023-04-29 16:41:56 -0400

    Added '%' functionality

--- VirtualMachine/src/calculator.rs
+++ VirtualMachine/src/calculator.rs
@@ -6,24 +6,25 @@ pub fn calculate(operation: &String, x: f64, y: f64) -> f64 {
         "/" => div(x, y),
         "^" => pow(x, y),
         "#" => x,
+        "%" => rem(x, y),
         _ => panic!("ERROR | Unexpected operator")
     }
 }
 
 fn add(x: f64, y: f64) -> f64 {
-    return x + y;
+    x + y
 }
 
 fn sub(x: f64, y: f64) -> f64 {
-    return x - y;
+    x - y
 }
 
 fn mul(x: f64, y: f64) -> f64 {
-    return x * y;
+    x * y
 }
 
 fn div(x: f64, y: f64) -> f64 {
-    return x / y;
+    x / y
 }
 
 fn pow(x: f64, y: f64) -> f64 {
@@ -34,5 +35,10 @@ fn pow(x: f64, y: f64) -> f64 {
         result *= x;
         index += 1;
     }
-    return result;
+    result
+}
+
+// added '%' operator functionality
+fn rem(x: f64, y: f64) -> f64 {
+    x % y
 }
