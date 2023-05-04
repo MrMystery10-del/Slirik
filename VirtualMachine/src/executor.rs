7a479a4fe77c8564b58b1daf92bde53b49a1cde6
Mr Mystery 1.0 <69641472+MrMystery10-del@users.noreply.github.com>
2023-05-04 18:39:06 +0200

    Added documentation to executor module
    Since its my code its better if i document it so new contributors can understand the code easier

--- VirtualMachine/src/executor.rs
+++ VirtualMachine/src/executor.rs
@@ -24,6 +24,7 @@ pub struct Storage {
     pub value: String,
 }
 
+// Execute a function based on provided statement
 pub fn execute(mut state: &mut State, statement: Statement) -> bool {
     let mut doSkip = false;
     match statement.identifier.as_str() {
@@ -48,6 +49,7 @@ pub fn execute(mut state: &mut State, statement: Statement) -> bool {
     doSkip
 }
 
+// Add a value to loaded value with current loaded operator
 fn add(state: &mut State, statement: Statement) {
     match &state.variable_type {
         Some(variable_type) =>
@@ -59,8 +61,10 @@ fn add(state: &mut State, statement: Statement) {
     }
 }
 
+// Calls a function and returns a value from that function
 fn call(state: &mut State, statement: Statement){}
 
+// Sets a new condition point, that can be a number or boolean value
 fn con(state: &mut State, statement: Statement) {
     match (state.condition.0.is_none(), state.condition.2.is_none()) {
         (true, _) => state.condition.0 = Some(statement.value),
@@ -72,6 +76,7 @@ fn con(state: &mut State, statement: Statement) {
     }
 }
 
+// Sets the condition operator, that can be: '<', '>' or '=='
 fn cop(state: &mut State, statement: Statement) {
     if state.condition.1.is_none() {
         state.condition.1 = Some(statement.value);
@@ -80,14 +85,17 @@ fn cop(state: &mut State, statement: Statement) {
     }
 }
 
+// Sets the current directory (unused before function implementation)
 fn dir(state: &mut State, statement: Statement) {
     state.directory = statement.value
 }
 
+// Clears the current condition after out of scout
 fn end(state: &mut State) {
     clear_condition(state)
 }
 
+// Gets a value from variable
 fn get(state: &mut State, statement: Statement) {
     match &state.loaded_variable {
         Some(..) => {
@@ -99,6 +107,7 @@ fn get(state: &mut State, statement: Statement) {
     }
 }
 
+// Loads a variable
 fn load(state: &mut State, statement: Statement) {
     if state.variables.contains(&(state.class.clone(), state.directory.clone(), statement.value.clone())) {
         state.loaded_variable = Some(statement.value.clone());
@@ -109,16 +118,21 @@ fn load(state: &mut State, statement: Statement) {
     }
 }
 
+// Loads a new operant which will be used on next value adding
 fn op(state: &mut State, statement: Statement) {
     state.operation = statement.value
 }
 
+// Adds a new parameter for function which will be required on function call
 fn param(state: &mut State, statement: Statement){}
 
+// Returns a value to last caller
 fn return_(state: &mut State, statement: Statement){}
 
+// Sets the return type of a function
 fn reva(state: &mut State, statement: Statement){}
 
+// Sets a new value to loaded value
 fn set(state: &mut State, statement: Statement) {
     match &state.loaded_variable {
         Some(loaded_variable) => {
@@ -132,14 +146,18 @@ fn set(state: &mut State, statement: Statement) {
     }
 }
 
+// Check if condition is true or false, based on that returns a boolean value to decide if should
+// skip until next end statement
 fn skip(state: &mut State) -> bool {
     !check_condition(state)
 }
 
+// Sets the type of variables created after
 fn setType(state: &mut State, statement: Statement) {
     state.variable_type = Some(statement.value);
 }
 
+// Creates a new variable
 fn var(state: &mut State, statement: Statement) {
     let value = &statement.value;
     if !state.variables.contains(&(state.class.clone(), state.directory.clone(), value.clone())) {
@@ -158,6 +176,7 @@ fn var(state: &mut State, statement: Statement) {
     }
 }
 
+// Prints all variables with their values (just for testing)
 pub fn print_variables(state: State) {
     for val in state.variable_value {
         println!("{} {} {}", val.1.value_type, val.0, val.1.value);
