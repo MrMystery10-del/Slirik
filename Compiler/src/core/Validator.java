929b56001d6dd92d2a61c54f1dc1ca1a886f13be
nandofromthebando <87321214+nandofromthebando@users.noreply.github.com>
2023-06-02 10:20:49 -0700

    Issue "#17"
    Adding documentation to Validator

--- Compiler/src/core/Validator.java
+++ Compiler/src/core/Validator.java
@@ -8,19 +8,25 @@ import java.util.Queue;
 import java.util.Set;
 
 public class Validator {
-
+    // Queue to sotre validated statements
     private Queue<Statement> validated = new LinkedList<>();
+    //Queue to store non-validated statements
     private Queue<Statement> nonValidated;
 
+    // Set to store variables ecountered during the validation proccess
     private Set<String> variables = new HashSet<>();
+    // Directory, type, and operator used for validation
     private String directory = "";
     private String type = "";
     private String operator = "";
 
+    // Constructor for validator object eith a queue of statements that need ot be validated
+    // Paramenter is the queue of statements to be validated
     protected Validator(Queue<Statement> statements) {
         this.nonValidated = statements;
     }
 
+    // Returns the queue of validated statements
     protected Queue<Statement> getValidatedStatements() {
         while (!nonValidated.isEmpty()) {
             Statement statement = nonValidated.poll();
@@ -36,6 +42,7 @@ public class Validator {
         return validated;
     }
 
+    //Validates the directory statement and updates the directory value as neccessary
     private void validateDir(Statement statement, String value) {
         if (!directory.equals(value)) {
             validated.offer(statement);
@@ -43,6 +50,7 @@ public class Validator {
         }
     }
 
+    // Validates the operator statement and updates the operator value as neccesarry
     private void validateOp(Statement statement, String value) {
         if (!operator.equals(value)) {
             validated.offer(statement);
@@ -50,6 +58,7 @@ public class Validator {
         }
     }
 
+    // Validates the type statememnt and updates the type value as neccessarry
     private void validateType(Statement statement, String value) {
         if (!type.equals(value)) {
             validated.offer(statement);
@@ -57,6 +66,7 @@ public class Validator {
         }
     }
 
+    // Validates the variable statement and adds it to the set of variables f it is unique
     private void validateVar(Statement statement, String value) {
         if (variables.add(value)) {
             validated.offer(statement);
